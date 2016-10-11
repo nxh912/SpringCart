5aa041698438092a0842d06e2a2aeda56a664960
NL <nxh912@users.noreply.github.com>
2016-10-11 09:47:10 +0800

    Create Cart.java

---
+++ src/main/java/com/springcart/model/Cart.java
@@ -0,0 +1,28 @@
+package com.springcart.model;
+
+import java.util.ArrayList;
+
+public class Cart {
+	private int cartId;
+	private float cartTotal;
+	private ArrayList<LineItem> lineItems;
+	
+	public Cart(int cartId) {
+		this.cartId = cartId;
+		this.lineItems = new ArrayList<LineItem> ();
+	}
+	
+	public int getCartId() {
+		return this.cartId;
+	}
+	
+	public float getCartTotal() {
+		// Return cart total
+		return this.cartTotal;
+	}
+	
+	public ArrayList<LineItem> getLineItems() {
+		return this.lineItems;
+	}
+		
+}
